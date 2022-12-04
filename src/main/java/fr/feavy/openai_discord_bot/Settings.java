a82b0a2a095b46eb8dd2bd253b7c592eefa78c82
Feavy <34109688+Feavy@users.noreply.github.com>
2022-12-04 10:18:08 +0100

    Text engine env variable

--- src/main/java/fr/feavy/openai_discord_bot/Settings.java
+++ src/main/java/fr/feavy/openai_discord_bot/Settings.java
@@ -1,6 +1,6 @@
 package fr.feavy.openai_discord_bot;
 
 public class Settings {
-    public final static String ENGINE = "text-davinci-003";
+    public final static String ENGINE = System.getEnv("ENGINE");
     // text-curie-001
 }
