edbe90989e3077fd310e35983c58980aab691656
Feavy <34109688+Feavy@users.noreply.github.com>
2022-12-04 10:22:42 +0100

    Fi

--- src/main/java/fr/feavy/openai_discord_bot/Settings.java
+++ src/main/java/fr/feavy/openai_discord_bot/Settings.java
@@ -1,6 +1,6 @@
 package fr.feavy.openai_discord_bot;
 
 public class Settings {
-    public final static String ENGINE = System.getEnv("ENGINE");
+    public final static String ENGINE = System.getenv("ENGINE");
     // text-curie-001
 }
