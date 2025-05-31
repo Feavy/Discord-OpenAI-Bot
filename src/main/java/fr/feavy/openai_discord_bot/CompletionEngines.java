65c43fda2c4f3f812cf9d4341be8cba40fe1a6a7
Feavy <34109688+Feavy@users.noreply.github.com>
2025-05-31 21:15:28 +0200

    Add engines

--- src/main/java/fr/feavy/openai_discord_bot/CompletionEngines.java
+++ src/main/java/fr/feavy/openai_discord_bot/CompletionEngines.java
@@ -15,11 +15,18 @@ public class CompletionEngines {
         COMMON_ENGINES.put("gpt3+", new CompletionEngine("gpt-3.5-turbo-16k", true, false));
         COMMON_ENGINES.put("gpt4+", new CompletionEngine("gpt-4-32k", true, false));
         COMMON_ENGINES.put("gpt4o", new CompletionEngine("gpt-4o", true, true));
+        COMMON_ENGINES.put("gpt4o", new CompletionEngine("gpt-4o", true, true));
+        COMMON_ENGINES.put("o3mini", new CompletionEngine("o3-mini", true, true));
+        COMMON_ENGINES.put("o4mini", new CompletionEngine("o4-mini", true, true));
     }
 
     public static final CompletionEngine DEFAULT = COMMON_ENGINES.get("gpt4o");
 
     public static CompletionEngine get(String name) {
-        return COMMON_ENGINES.get(name);
+        CompletionEngine engine = COMMON_ENGINES.get(name);
+        if(engine == null) {
+            return new CompletionEngine(name, true, true)
+        }
+        return engine;
     }
 }
